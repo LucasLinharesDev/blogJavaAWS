be5d7bd4749e54292675b0d197c573e93e87b1d5
Lucas Linhares <lucas.bsb.12@gmail.com>
2022-09-03 17:32:50 -0300

    primeiro commit

---
+++ src/main/java/com/spring/blogJavaAWS/BlogJavaAwsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.blogJavaAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJavaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJavaAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/blogJavaAWS/BlogJavaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogJavaAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJavaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
